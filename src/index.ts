3c0e538ca342a71c4e3829630c7386424e3a57b7
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-24 00:04:33 +0100

    v0.1.925

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,10 @@ class MicromarkTool {
     highlight(): void {
         document.querySelectorAll<HTMLDivElement>('div[class^="shj-lang-"]').forEach((elm) => {
             const lang = (/shj-lang-([^\s]+)/.exec(elm.className) || [])[1];
-            if (lang) highlightElement(elm, 'js', 'multiline', { hideLineNumbers: true });
+            if (lang) {
+                highlightElement(elm, 'js', 'multiline', { hideLineNumbers: true });
+                elm.style.setProperty('font-family', "'Fira Code', 'Fira Mono', monospace", 'important');
+            }
         });
     }
 
